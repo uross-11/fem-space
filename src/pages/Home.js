7235afd1b175687e64fb26c6b2a23c784e92f390
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-11-12 01:52:25 +0100

    Add homepage content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>SO, YOU WANT TO TRAVEL TO <span>SPACE</span></h1>
+      <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      <button>EXPLORE</button>
     </div>
   );
 }
