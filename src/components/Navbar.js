84a6113dc39996e7fe3ec561691de81c9f595825
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-11-15 22:48:41 +0100

    Add homepage mobile style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/shared/logo.svg'
+import hamburger from '../assets/shared/icon-hamburger.svg'
 
 const Navbar = () => {
   return (
-    <nav className='header'>
-      <img className='header__logo' src="./assets/shared/logo.svg" alt="" />
-      <div className='header__line'></div>
-      <div className='header__links nav'>
+    <nav className='header container'>
+      <img className='header__logo' src={logo} alt={`logo-img`} />
+      <div className='header__line hide-for-mobile'></div>
+      <div className='header__links hide-for-mobile nav'>
+        <Link to='/' className='header__links__link'><span>01</span> HOME</Link>
+        <Link to='/destination' className='header__links__link'><span>02</span> DESTINATION</Link>
+        <Link to='/crew' className='header__links__link'><span>03</span> CREW</Link>
+        <Link to='/technology' className='header__links__link'><span>04</span> TECHNOLOGY</Link>
+      </div>
+      <div className='line'></div>
+      <button className='header__toggle'>
+        <img src={hamburger} alt={`hamburger-img`} />
+      </button>
+      {/* fix hide-for-mobile */}
+      <div className='header__mobile hide-for-mobile nav'>
         <Link to='/' className='header__links__link'><span>01</span> HOME</Link>
         <Link to='/destination' className='header__links__link'><span>02</span> DESTINATION</Link>
         <Link to='/crew' className='header__links__link'><span>03</span> CREW</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h5>SO, YOU WANT TO TRAVEL TO</h5>
-      <h1>SPACE</h1>
-      <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-      <button>EXPLORE</button>
+    <div className='home container'>
+      <div className='home__background'></div>
+      <h5 className='hide-for-mobile'>SO, YOU WANT TO TRAVEL TO</h5>
+      <h1 className='home__title'>SPACE</h1>
+      <p className='home__text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      <button className='home__cta explore'>EXPLORE</button>
     </div>
   );
 }
