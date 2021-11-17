e91d760fdf915e3827f25dc4f8e7b9cdcb4de7d5
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-11-18 00:22:21 +0100

    Add technology page mobile style

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -6,26 +6,26 @@ const Technology = ({ technology }) => {
   const tech = technology[techIndex];
 
   const check = tech.name.toLowerCase().split(' ')[0];
-  const orientation = 'portrait'
+  const orientation = 'landscape'
 
   const images = useImportImages();
   const image = useSwitchImage(images, check, orientation);
 
   return (
-    <div>
-      <h5><span>03</span> SPACE LAUNCH 101</h5>
+    <div className='technology'>
+      <h5 className='technology__h5 container'><span>03</span> SPACE LAUNCH 101</h5>
       <section>
-        <img src={image} alt={`${tech.name}-img`} />
+        <img className='technology__image' src={image} alt={`${tech.name}-img`} />
         <div>
-          <div>
-            <button onClick={() => {setTechIndex(0)}}>1</button>
-            <button onClick={() => {setTechIndex(1)}}>2</button>
-            <button onClick={() => {setTechIndex(2)}}>3</button>
+          <div className='technology__select'>
+            <button className='technology__select__link' onClick={() => {setTechIndex(0)}}>1</button>
+            <button className='technology__select__link' onClick={() => {setTechIndex(1)}}>2</button>
+            <button className='technology__select__link' onClick={() => {setTechIndex(2)}}>3</button>
           </div>
-          <div>
-            <span className='nav'>THE TERMINOLOGY …</span>
-            <h3>{tech.name}</h3>
-            <p>{tech.description}</p>
+          <div className='container'>
+            <span className='technology__term'>THE TERMINOLOGY…</span>
+            <h3 className='technology__name'>{tech.name.toUpperCase()}</h3>
+            <p className='technology__text'>{tech.description}</p>
           </div>
         </div>
       </section>
