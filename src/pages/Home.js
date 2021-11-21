ddcdd794a40e97f7d0850fa90f92b7b09ab2fc0a
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-11-21 13:23:52 +0100

    Add homepage tablet and desktop styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='home container'>
-      <h5 className='hide-for-mobile'>SO, YOU WANT TO TRAVEL TO</h5>
-      <h1 className='home__title'>SPACE</h1>
-      <p className='home__text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    <div className='home container__body'>
+      <div className="home__content">
+        <h5 className='home__h5 hide-for-mobile'>SO, YOU WANT TO TRAVEL TO</h5>
+        <h1 className='home__title'>SPACE</h1>
+        <p className='home__text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      </div>
       <button className='home__cta explore'>EXPLORE</button>
     </div>
   );
