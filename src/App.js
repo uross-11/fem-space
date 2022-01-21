27ee00d3e891f1235e12f2e96139650646a8c796
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2022-01-21 18:06:38 +0100

    Fix app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter as Route, Routes, useLocation } from "react-router-dom";
 import { useTransition, animated } from 'react-spring';
 
 import data from './data.json';
